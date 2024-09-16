75e7480d6f006f83c7c7fd7d3913f4af2246f627
Jac <39693487+jac0x@users.noreply.github.com>
2024-09-16 15:31:08 -0400

    feat: change tokenbalance to tokenholder

--- src/lube.ts
+++ src/lube.ts
@@ -1,73 +1,21 @@
 import {
-  Approval as ApprovalEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
 } from "../generated/LUBE/LUBE"
-import { Approval, OwnershipTransferred, Transfer } from "../generated/schema"
 
-/*export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransferO(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}*/
-
-
-//import entities
-import {TokenBalance} from "../generated/schema"
+import {TokenHolder} from "../generated/schema"
 //import the functions defined in utils.ts
 import {
   fetchTokenDetails,
   fetchAccount,
-  fetchBalance
 } from "./utils"
 //import datatype
 import { BigDecimal} from "@graphprotocol/graph-ts";
 
 export function handleTransfer(event: TransferEvent): void {
     let token = fetchTokenDetails(event);
-    if (!token) { //if token == null
-        return
-      }
+    if (!token) {
+      return;
+    }
 
     //get account addresses from event
     let fromAddress = event.params.from.toHex();
@@ -76,4 +24,28 @@ export function handleTransfer(event: TransferEvent): void {
     //fetch account details
     let fromAccount = fetchAccount(fromAddress);
     let toAccount = fetchAccount(toAddress);
+
+    if (!fromAccount || !toAccount) {
+      return;
+    }
+
+    let fromId = `${token.id}-${fromAccount.id}`
+    let fromTokenHolder = TokenHolder.load(fromId);
+    if (!fromTokenHolder) {
+      fromTokenHolder = new TokenHolder(fromId);
+      fromTokenHolder.token = token.id;
+      fromTokenHolder.account = fromAccount.id;
+
+      fromTokenHolder.save();
+    }
+
+    let toId = `${token.id}-${toAccount.id}`
+    let toTokenHolder = TokenHolder.load(toId);
+    if (!toTokenHolder) {
+      toTokenHolder = new TokenHolder(toId);
+      toTokenHolder.token = token.id;
+      toTokenHolder.account = toAccount.id;
+
+      toTokenHolder.save();
+    }
 }
